3c7acea6bd86f00d5e2b725bf4e5d2aeffc2e575
teddy <a21506qoo@gmail.com>
2021-05-11 21:37:20 +0800

    fix: bugfixed: delete formCheck in addnew.js

--- routes/modules/addnew.js
+++ routes/modules/addnew.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
-const formSubmitCheck = require('../../public/javascripts/formSubmitCheck')
 
 router.get('/', (req, res) => {
   return res.render('new')
@@ -11,7 +10,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   //  回傳資料
   // 檢查是否有缺少資料，是的話則跳出錯誤
-  
+
   return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))

7e6a7cd9ddb6d57702a260e3a2a7c96aea9e71a1
teddy <a21506qoo@gmail.com>
2021-05-11 15:10:42 +0800

    refactor: reconstruct routes: delete sort and search and add them into home

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-const search = require('./modules/search')
 const addnew = require('./modules/addnew')
-const sort = require('./modules/sort')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
-router.use('/search', search)
 router.use('/addnew', addnew)
-router.use('/sort', sort)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Restaurant = require('../../models/restaurant')
+const sortMethod = require('../../public/javascripts/sortMethod')
 
 router.get('/', (req, res) => {
   Restaurant.find()
@@ -9,5 +10,31 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
+
+// sort function
+router.get('/sort', (req, res) => {
+  const sortSelected = req.query.sort
+  const sortRule = sortMethod(sortSelected)
+  Restaurant.find()
+    .lean()
+    .sort(sortRule)
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// search function
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  return Restaurant.find({
+    $or: [
+      { name: { $regex: `${keyword}`, $options: '$i' } },
+      { category: { $regex: `${keyword}`, $options: '$i' } }
+    ]
+  })
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(error => console.error(error))
+})
+
 
 module.exports = router
